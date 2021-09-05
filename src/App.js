fd3e4c5934467645ed2bb20961f39dfde0585b7d
aahzambardai <ahzam.bardai@outlook.com>
2021-09-05 10:05:40 -0500

    merge with zavier

--- src/App.js
+++ src/App.js
@@ -2,6 +2,8 @@ import './App.css';
 import Landing from './components/Landing'
 import { Route , Switch } from 'react-router-dom'
 import Login from './components/Login';
+import Dashboard from './components/Dashboard'
+
 
 function App() {
   return (
@@ -11,15 +13,11 @@ function App() {
     <Switch>
     <Route exact path="/Login" component ={Login}/>
     
-    
-   
-    
     <Landing/>
     
     </Switch>
-    
-    
-     
+      <Route  exact path = '/dashboard'
+              component = { Dashboard } />
     </div>
   );
 }
