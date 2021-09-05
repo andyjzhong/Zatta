41446cebeaa63afd5356ed891c6fbff403d5dd50
aahzambardai <ahzam.bardai@outlook.com>
2021-09-05 10:08:31 -0500

    merge with zavier

--- src/App.js
+++ src/App.js
@@ -2,11 +2,30 @@ import './App.css';
 import Landing from './components/Landing'
 import { Route , Switch } from 'react-router-dom'
 import Login from './components/Login';
-import Dashboard from './components/Dashboard'
-import MainApp from './components/mainApp';
 
+import React, { useState, useEffect } from 'react'
+import Dashboard from './components/Dashboard/Dashboard.jsx'
+import MainApp from './components/Main App/MainApp';
+import axios from 'axios'
 
 function App() {
+
+  const [files, setFiles] = useState([])                      //setting state and variable for the files
+  const [user, setUser] = useState('')
+  const [note, setNote] = useState('')
+  const urlNotes = "https://zatta1.herokuapp.com/api/notes/"
+  const urlUser = `https://zatta1.herokuapp.com/api/users/${user}`
+
+
+  useEffect(() => {                                           //fetching backend data
+    
+    axios.get(urlNotes).then((res) => {
+        setFiles(res.data)
+        
+    })
+    
+  }, [])
+
   return (
     <div class='bg-gray-50' >
       
@@ -19,8 +38,9 @@ function App() {
     </Switch>
       <Route  exact path = '/dashboard'
               component = { Dashboard } />
+      <Route  exact path = '/dashboard' component = { () => <Dashboard files={files} /> } />
 
-      <MainApp />
+      <Route exact path = '/notes/:id' component={(routerProps) => <MainApp note={routerProps.match.params.id} /> } />
     </div>
   );
 }
