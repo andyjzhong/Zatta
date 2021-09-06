6b6d7062e58a034ee7284aa2b8a18e9e0b4f335d
aahzambardai <ahzam.bardai@outlook.com>
2021-09-05 22:21:23 -0500

    Routes working with Login

--- src/App.js
+++ src/App.js
@@ -1,41 +1,40 @@
-import './App.css';
-import Landing from './components/Landing/Landing'
-import { Route , Switch } from 'react-router-dom'
-import Login from './components/login/Login';
+import { Route, Redirect } from 'react-router-dom'
+import React, { useEffect } from 'react'
+import axios from 'axios'
 
-import React, { useState, useEffect } from 'react'
+// Importing Components
+import Landing from './components/Landing/Landing'
+import Login from './components/login/Login';
 import Dashboard from './components/Dashboard/Dashboard.jsx'
 import MainApp from './components/Main App/MainApp';
-import axios from 'axios'
+import userStore  from './components/Users/GetUsers';
+
 
 function App() {
 
-  const [files, setFiles] = useState([])                      //setting state and variable for the files
-  const [user, setUser] = useState('')
-  const [note, setNote] = useState('')
   const urlNotes = "https://zatta1.herokuapp.com/api/notes/"
-  const urlUser = `https://zatta1.herokuapp.com/api/users/${user}`
+  const urlUsers = "https://zatta1.herokuapp.com/api/users/"
+  const setUsers = userStore(state => state.setUsers)
+  const setNotes = userStore(state => state.setNotes) 
+  const isLoggedIn = userStore(state => state.isLoggedIn)
 
+  useEffect(() => {
+    axios.get(urlUsers).then(res => setUsers(res.data)) // store all users in the Zustand store
+    axios.get(urlNotes).then((res) => setNotes(res.data))  // Get all notes
 
-  useEffect(() => {                                           //fetching backend data
-    
-    axios.get(urlNotes).then((res) => {
-        setFiles(res.data)
-        
-    })
-    
   }, [])
 
   return (
-    <div class='bg-gray-50' >
+    <div >
       
       <Route exact path='/' component={Landing} />
 
-      <Route exact path="/Login" component ={Login}/>
+      <Route exact path="/login" component={ routerProps => <Login history={routerProps.history} />}/>
     
-      <Route  exact path = '/dashboard' component = { () => <Dashboard files={files} /> } />
+      <Route  exact path = '/dashboard' component = { (routerProps) => isLoggedIn ? <Dashboard history={routerProps.history} /> : <Login {...routerProps.history.push('/login')} /> } />
 
-      <Route exact path = '/notes/:id' component={(routerProps) => <MainApp note={routerProps.match.params.id} /> } />
+      <Route exact path = '/notes/:id' component={(routerProps) => isLoggedIn ? <MainApp note={routerProps.match.params.id} /> : <Login />} /> 
+
     </div>
   );
 }
